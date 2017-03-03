5e855e8f09ec7991b57bb6d942b9d6e0362b96b8
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-03-03 16:01:10 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Recipe.create([{name: "Vegetable Soup"}, {name: "Stir-Fry"}])
+Ingredient.create([{name: "Tomato"}, {name: "Corn"}, {name: "Celery"}])
